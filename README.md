6f8a841438df48b70525dcdd07ccf46660ccc534
BanhBao <BanhBao@apples-iMac.local>
2016-05-05 11:59:54 +0700

    tungns commit setup

---
+++ README.md
@@ -0,0 +1,22 @@
+# chat-private-multi-users
+
+Simple example to demostrate socket.io 
+
+## How to install ?
+
+
+* Socket.io (npm install socket.io)
+
+## How to run the application ?
+
+run the node server by typing 
+
+```
+node app.js
+```
+
+By default the app runs on port 3000, type in the url 
+http://localhost:3000
+
+
+###### Enjoy the chat 
---
+++ server.js
@@ -0,0 +1,53 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(3000);
+
+var clients = {};
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  console.info('socket.id',socket.id);
+  socket.on('add-user', function(data){
+    clients[data.username] = {
+      "socket": socket.id
+    };
+  });
+
+  socket.on('private-message', function(data){
+    console.log("Sending: " + data.content + " to " + data.username);
+    console.info('clients[data.username]',clients[data.username]);
+    if (clients[data.username]){
+      io.sockets.connected[clients[data.username].socket].emit("add-message", data);
+    } else {
+      console.log("User does not exist: " + data.username); 
+    }
+  });
+
+  //Removing the socket on disconnect
+  socket.on('disconnect', function() {
+  	for(var name in clients) {
+  		if(clients[name].socket === socket.id) {
+  			delete clients[name];
+  			break;
+  		}
+  	}	
+  })
+
+});
+
+
+
